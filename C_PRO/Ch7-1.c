c997a8c74b1dd619bf08bd253eb597a6d52373cf
hwawon <hwa2won@gmail.com>
2021-05-11 21:13:44 +0900

    초기 커밋.

---
+++ C_PRO/Ch7-1.c
@@ -0,0 +1,19 @@
+//예제 7-1. 배열의 바이트 크기와 배열의 크기 구하기
+#include <stdio.h>
+int main()
+{
+	int arr[5];
+	int size = 0;
+	int i;
+
+	printf("배열의 바이트 크기 : %d\n", sizeof(arr));
+
+	size = sizeof(arr) / sizeof(arr[0]);
+	printf("배열의 크기 : %d\n", size);
+
+	for (i = 0; i < size; i++)
+		arr[i] = 0;
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
