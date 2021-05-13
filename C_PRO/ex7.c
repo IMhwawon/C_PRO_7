810a8dbd00d417c83714142688ef954fe7964466
hwawon <hwa2won@gmail.com>
2021-05-13 10:29:11 +0900

    연습문제 7. 실수형 배열 원소 출력

---
+++ C_PRO/ex7.c
@@ -0,0 +1,11 @@
+//연습문제 7. 실수형 배열 원소 출력
+#include <stdio.h>
+
+int main()
+{
+	double values[] = { 0.5, 0.125, 0.75, 1.05, 2.25, 3.75 };
+	int count = sizeof(values) / sizeof(values[0]);
+	int i;
+	for (i = 0; i < count; i++)
+		printf("%f ", values[i]);
+}
